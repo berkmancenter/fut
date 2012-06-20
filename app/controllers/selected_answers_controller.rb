5769efe72d10d0d91b37f25340d28e85ca6a7ab2
Karim Husseiny <just.azzurri@gmail.com>
2012-06-21 01:30:31 +0200

    Back button feature for editing previous answer

--- app/controllers/selected_answers_controller.rb
+++ app/controllers/selected_answers_controller.rb
@@ -6,13 +6,17 @@ class SelectedAnswersController < ApplicationController
 		if (@question_id = @selected_answer.question.id) != 6 
 			#redirect_to "/questions/show/#{@question_id + 1}"
 			respond_to do |format|
-				format.html { redirect_to "/questions/show/#{@question_id + 1}" }
 				format.js  
 			end
-
-
 		else
-			redirect_to "/tests/show/#{@selected_answer.test.id}"		
+			 redirect_to "/tests/show/#{@selected_answer.test.id}"				
+		end
+	end
+
+	def edit
+		@selected_answer= SelectedAnswer.find(params[:id])
+		respond_to do |format|
+			format.js  
 		end
 	end
 end
