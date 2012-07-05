77de469d50cc2dd3a2359878b00a869ec8284873
Karim Husseiny <just.azzurri@gmail.com>
2012-07-05 03:26:00 +0200

    define get_result method in test_answer model and improve case_answer

--- app/controllers/case_answers_controller.rb
+++ app/controllers/case_answers_controller.rb
@@ -10,8 +10,6 @@ class CaseAnswersController < ApplicationController
 	def show
 		@case_answer = CaseAnswer.find params[:id]
 		@case = Case.find_by_title params[:case_id]
-		@result = @case_answer.test_answer.result ? "Fair Use" : "Not Fair Use"
-		@court_result = @case.court_decision.result ? "Fair Use" : "Not Fair Use"
 	end
 
 	def index
