f031c46dd20ad44b9e295917d9edc8d6e8a8a2c3
Karim Husseiny <just.azzurri@gmail.com>
2012-07-25 11:37:43 +0200

    configurations for heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Fut::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -51,6 +51,7 @@ Fut::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = {:host => 'fairusetool.heroku.com'}
   # Enable threaded mode
   # config.threadsafe!
 
