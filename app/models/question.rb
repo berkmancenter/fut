7c69ecbadb1cfe78f2305a58ade1f256716007ab
Karim Husseiny <just.azzurri@gmail.com>
2012-08-07 18:33:29 +0200

    fix questions validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ActiveRecord::Base
   has_many :answers
   belongs_to :aspect
   attr_accessible :content, :explanation, :aspect_id
-  validates :content, :presence =>true, :length => { :maximum => 80 }
+  validates :content, :presence =>true, :length => { :maximum => 200 }
   validates :explanation, :length => { :maximum => 600 }
   scope :essential , where(:aspect_id => nil)
   scope :quiz , where(:aspect_id => 1..6 )
