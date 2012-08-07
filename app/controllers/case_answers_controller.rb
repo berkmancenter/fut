78648aa3320906c98248427ce7fbb1c2e904f89e
Karim Husseiny <just.azzurri@gmail.com>
2012-08-07 19:14:09 +0200

    Order selected Q&A

--- app/controllers/case_answers_controller.rb
+++ app/controllers/case_answers_controller.rb
@@ -9,7 +9,7 @@ class CaseAnswersController < ApplicationController
 			redirect_to home_path
 		else
 			@facts = @legal_case.facts.split(". ") 
-			@questions = Question.essential
+			@questions = Question.essential.order('id')	
 			@test_answer = TestAnswer.new
 		end	
 	end
@@ -20,7 +20,7 @@ class CaseAnswersController < ApplicationController
 		@case_answer= @legal_case.case_answers.find params[:id]
 		@court_answers_report = @legal_case.get_report
 		@answers_of_case = @case_answer.test_answer.get_answers
-		@questions= Question.essential
+		@questions= Question.essential.order('id')	
 	end
 
 	#We don't want these two actions anymore
--- db/migrate/20120807133527_create_aspects.rb
+++ db/migrate/20120807133527_create_aspects.rb
@@ -2,7 +2,7 @@ class CreateAspects < ActiveRecord::Migration
   def change
     create_table :aspects do |t|
     	t.string :name
-    	t.string :explanation	
+    	t.text :explanation	
       	t.timestamps
     end
   end
