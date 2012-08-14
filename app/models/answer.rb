bce33e3953d6fceab6f31f3bf76642af99870633
Karim Husseiny <just.azzurri@gmail.com>
2012-08-14 08:20:52 +0200

    Fix the association between aspect and question models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   has_many :test_answers
-  attr_accessible :content, :explanation, :value, :question_id
+  attr_accessible :content, :hint, :value, :question_id
   validates :content, :presence =>true, :length => { :maximum => 80 }
   validates :value, :inclusion => { :in => -1..1 }
 end
